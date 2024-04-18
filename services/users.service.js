a954ed053dd0ff6610e3b8b8d648a79de0b6d580
Kirill Agishin <kirillagishin@ya.ru>
2024-04-18 13:24:45 +0500

    feat: add register method for user service #86byddfgw

--- services/users.service.js
+++ services/users.service.js
@@ -1,4 +1,4 @@
-import {Sequelize} from "sequelize";
+import {DataTypes, Sequelize} from "sequelize";
 
 module.exports = {
 	name: "users",
@@ -17,15 +17,46 @@ module.exports = {
 			"creditCard": "string",
 		}
 	},
-	actions: {
-
-	},
+	actions: {},
 	methods: {
+		createUser(user) {
 
+		}
 	},
-	created() {
+	async created() {
 		this.db = new Sequelize(process.env.DATABASE_URL, {
 			database: process.env.DATABASE_NAME,
+			dialect: "postgres",
+		});
+		try {
+			await this.db.authenticate();
+			console.log("Connection has been established successfully.");
+		} catch (error) {
+			console.error("Unable to connect to the database:", error);
+			process.exit(1);
+		}
+		this.userRepository = this.db.define("user", {
+			id: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4,
+				primaryKey: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			city: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			address: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			credit_card: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			}
 		});
 	},
 	stopped() {
